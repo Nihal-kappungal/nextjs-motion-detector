ad9f9232c1fd588e8fc5db29f69a96faf0316ced
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-04 13:24:11 +0530

    component issue fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { GooeyToaster } from "goey-toast";
+import { Toaster } from "@/components/shake/Toaster";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <GooeyToaster position="top-center" />
+        <Toaster />
         {children}
       </body>
     </html>
